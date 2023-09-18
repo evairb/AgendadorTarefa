a3d03d7d9cb334775d023796901ed458e328cb28
@evairbd <evairbd@prefeitura.sp.gov.br>
2023-09-18 16:34:21 -0300

    adicionado detalhes da tarefa

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,6 +6,9 @@ from agenda import views
 app_name = 'agenda'
 
 urlpatterns = [
+    
     path('', views.ListaTarefas.as_view(), name='tarefa'),
+    path('detalhe/<pk>', views.DetalhesTarefa.as_view(), name='detalhe'),
+    
     
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from agenda import models
 
@@ -13,5 +14,11 @@ from django.http import HttpResponse
 
 class ListaTarefas(ListView):
     model = models.Tarefa
-    template_name = 'system/lista_tarefas.html'
+    template_name = 'agenda/lista_tarefas.html'
     context_object_name = 'tarefas'
+
+class DetalhesTarefa(DetailView):
+    model = models.Tarefa
+    template_name = 'agenda/detalhe.html'
+    context_object_name = 'tarefa'
+    slug_url_kwarg = 'pk'
